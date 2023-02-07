69825e7e577cd6f51a1eceb740bdead11051cebf
Sergey Zuev <ekzaton@gmail.com>
2023-02-08 03:04:01 +0500

    4. Added AppLayout component

---
+++ src/frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__logo">
+        <a href="" class="logo">
+          <img
+            src="../assets/img/logo.svg"
+            alt="V!U!E! Pizza logo"
+            width="90"
+            height="40"
+          />
+        </a>
+      </div>
+      <div class="header__cart">
+        <a href="">{{ totalPrice }} ₽</a>
+      </div>
+      <div class="header__user">
+        <a href="" class="header__login"><span>Войти</span></a>
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLayout",
+  props: {
+    totalPrice: {
+      required: true,
+      type: Number,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../assets/scss/layout/header.scss";
+</style>
