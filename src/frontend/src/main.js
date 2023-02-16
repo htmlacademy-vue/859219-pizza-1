76ea5c037917c4694c0433690447224bcd869f12
Sergey Zuev <ekzaton@gmail.com>
2023-02-16 10:42:43 +0500

    15. Added vuex instance and store modules

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 
 import "./plugins/ui";
 import router from "./router";
+import store from "./store";
 import App from "./App";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount("#app");
---
+++ src/frontend/src/store/modules/index.js
@@ -0,0 +1,11 @@
+import Auth from "./auth";
+import Builder from "./builder";
+import Cart from "./cart";
+import Orders from "./orders";
+
+export default {
+  Auth,
+  Builder,
+  Cart,
+  Orders,
+};
