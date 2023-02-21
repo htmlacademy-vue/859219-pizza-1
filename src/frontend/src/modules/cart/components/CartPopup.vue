d7ac528057622e2e6f5e8686e19a1f21a68900c8
Sergey Zuev <ekzaton@gmail.com>
2023-02-22 02:05:05 +0500

    18. Updated Cart view and cart components

---
+++ src/frontend/src/modules/cart/components/CartPopup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="popup">
+    <a class="close" @click.prevent="closePopup">
+      <span class="visually-hidden">Закрыть попап</span>
+    </a>
+    <div class="popup__title">
+      <h2 class="title">Спасибо за заказ</h2>
+    </div>
+    <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
+    <a class="popup__button" @click.prevent="closePopup">
+      <div class="button">Отлично, я жду!</div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartPopup",
+  methods: {
+    closePopup() {
+      this.$emit("close-popup");
+    },
+  },
+};
+</script>
