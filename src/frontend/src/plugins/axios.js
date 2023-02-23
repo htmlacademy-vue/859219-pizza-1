964a96823dc44851ef921362b275b5f071bc531c
Sergey Zuev <ekzaton@gmail.com>
2023-02-23 23:53:15 +0500

    21. Added axios and notifier plugins

---
+++ src/frontend/src/plugins/axios.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+  baseURL: "/api/",
+});
+
+axiosInstance.interceptors.response.use(
+  (response) => response.data,
+
+  (error) => {
+    const defaultMessage = "Возникла ошибка при выполнении запроса к серверу";
+
+    axiosInstance.$notifier.error(
+      error?.response?.data?.error?.message || defaultMessage
+    );
+
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
---
+++ src/frontend/src/plugins/notifier.js
@@ -0,0 +1,37 @@
+import Notification from "../common/enums/notification";
+
+export default class Notifier {
+  #store;
+
+  constructor(store) {
+    this.#store = store;
+  }
+
+  info(text) {
+    this.#store.dispatch("createNotification", {
+      text,
+      type: Notification.INFO,
+    });
+  }
+
+  success(text) {
+    this.#store.dispatch("createNotification", {
+      text,
+      type: Notification.SUCCESS,
+    });
+  }
+
+  error(text) {
+    this.#store.dispatch("createNotification", {
+      text,
+      type: Notification.ERROR,
+    });
+  }
+
+  warning(text) {
+    this.#store.dispatch("createNotification", {
+      text,
+      type: Notification.WARNING,
+    });
+  }
+}
