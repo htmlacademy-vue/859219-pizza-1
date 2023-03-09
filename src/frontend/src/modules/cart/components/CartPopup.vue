15edf0f01f2e818e23812ba149f7cf3dbcbb3082
Sergey Zuev <ekzaton@gmail.com>
2023-03-09 12:46:52 +0500

    33. Added popup animations

--- src/frontend/src/modules/cart/components/CartPopup.vue
+++ src/frontend/src/modules/cart/components/CartPopup.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="popup">
-    <a class="close" @click.prevent="closePopup">
-      <span class="visually-hidden">Закрыть попап</span>
-    </a>
-    <div class="popup__title">
-      <h2 class="title">Спасибо за заказ</h2>
+  <div class="overlay">
+    <div class="popup">
+      <a class="close" @click.prevent="closePopup">
+        <span class="visually-hidden">Закрыть попап</span>
+      </a>
+      <div class="popup__title">
+        <h2 class="title">Спасибо за заказ</h2>
+      </div>
+      <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
+      <a class="popup__button" @click.prevent="closePopup">
+        <div class="button">Отлично, я жду!</div>
+      </a>
     </div>
-    <p>Мы начали готовить Ваш заказ, скоро привезём его вам ;)</p>
-    <a class="popup__button" @click.prevent="closePopup">
-      <div class="button">Отлично, я жду!</div>
-    </a>
   </div>
 </template>
 
@@ -23,3 +25,15 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+.overlay {
+  position: fixed;
+  inset: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  z-index: 999;
+  padding: 40px;
+  overflow: auto;
+}
+</style>
